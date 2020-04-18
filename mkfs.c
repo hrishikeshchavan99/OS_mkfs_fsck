8ef679097f0e5f050223b102ff4b0616cb78b587
Hrishikesh <hrishikeshchavan99@gmail.com>
2020-04-19 00:19:17 +0530

    changes made

--- mkfs.c
+++ mkfs.c
@@ -8,7 +8,7 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <sys/stat.h>
-
+#include <time.h>
 
 /*
 changes
@@ -24,6 +24,15 @@ change karat nahiye ata he
 8. use getopt
 option -b, using that set it to the bs
 */
+int ispowerof(int n, int k){
+	while (n != 1){
+		if(n % k != 0)
+        		return 0;
+		n = n / k;
+	}
+    	return 1;
+}
+
 int main(int argc, char *argv[]) {
 	int fd = open(argv[1], O_RDONLY); // argv[1] = /dev/sdb1 
 	
@@ -33,6 +42,7 @@ int main(int argc, char *argv[]) {
 	struct ext2_inode inode;
 	struct ext2_dir_entry_2 dirent;
 	unsigned int block_size;
+//	time_t T= time(NULL);
 
 	if(fd == -1) {
 		perror("error:");
@@ -49,10 +59,10 @@ int main(int argc, char *argv[]) {
 //block size and partition size is fixed only;
 	
 	for (i = 0; i < no_of_groups; i++){
-		if (i == 0 || i == 1 || i % 3 == 0 || i % 5 == 0 || i % 7 == 0){		
+		if (i == 0 || i == 1 || ispowerof(i, 3) || ispowerof(i, 3) || ispowerof(i, 3)){		
 	
-			sb.s_inodes_count = sb.s_inodes_per_group * no_of_bgroups;		/* Inodes count */
-			sb.s_blocks_count = p.size / bs;				/* Blocks count */
+			sb.s_inodes_count = sb.s_inodes_per_group * no_of_bgroups;		
+			sb.s_blocks_count = p.size / bs;				
 
 
 
@@ -67,14 +77,14 @@ int main(int argc, char *argv[]) {
 			no_of_groups = sb.s_blocks_count / sb.s_blocks_per_group;	
 
 //-11 right shift	
-			sb.s_clusters_per_group = 32768;	/* # Fragments per group */
+			sb.s_clusters_per_group = sb.s_blocks_per_group;	/* # Fragments per group */
 		
 			inode_bpg = bs / 8;
 	
 		--	sb.s_inode_size = 256;		/* size of inode structure */
 			sb.s_inodes_per_group = inode_bpg * bs / sb.s_inode_size;	/* # Inodes per group */
 			sb.s_mtime = 0;		/* Mount time */
-			sb.s_wtime;		/* Write time */
+			sb.s_wtime = time(NULL);		/* Write time */
 	
 //time null
 
@@ -101,83 +111,84 @@ int main(int argc, char *argv[]) {
 --			sb.s_first_ino = 11;		/* First non-reserved inode */
 
 			sb.s_block_group_nr = 0 or 1;	/* block group # of this superblock */
-			sb.s_feature_compat;	/* compatible feature set */
-			sb.s_feature_incompat;	/* incompatible feature set */
-			sb.s_feature_ro_compat;	/* readonly-compatible feature set */
+			sb.s_feature_compat = 0;	/* compatible feature set */
+			sb.s_feature_incompat = 0;	/* incompatible feature set */
+			sb.s_feature_ro_compat = 0;  	/* readonly-compatible feature set */
 			sb.s_uuid[16];		/* 128-bit uuid for volume */
 			char	s_volume_name[EXT2_LABEL_LEN];	/* volume name */
 			char	s_last_mounted[64];	/* directory where last mounted */
-			s_algorithm_usage_bitmap; /* For compression */
+			s_algorithm_usage_bitmap = 0; /* For compression */
 	
 
 
-			sb.s_prealloc_blocks;	/* Nr of blocks to try to preallocate*/
-			sb.s_prealloc_dir_blocks;	/* Nr to preallocate for dirs */
+			sb.s_prealloc_blocks = 0;	/* Nr of blocks to try to preallocate*/
+			sb.s_prealloc_dir_blocks = 0;	/* Nr to preallocate for dirs */
 			sb.s_reserved_gdt_blocks = 127;	/* Per group table for online growth */
 
-			sb.s_journal_uuid[16];	/* uuid of journal superblock */				
-			sb.s_journal_inum;		/* inode number of journal file */
-			sb.s_journal_dev;		/* device number of journal file */
-			sb.s_last_orphan;		/* start of list of inodes to delete */
-			sb.s_hash_seed[4];		/* HTREE hash seed */
-			sb.s_def_hash_version;	/* Default hash version to use */
-			sb.s_jnl_backup_type; 	/* Default type of journal backup */		
-			sb.s_desc_size;		/* Group desc. size: INCOMPAT_64BIT */
-			sb.s_default_mount_opts;
-			sb.s_first_meta_bg;	/* First metablock group */
-			sb.s_mkfs_time;		/* When the filesystem was created */
-			sb.s_jnl_blocks[17]; 	/* Backup of the journal inode */
-			sb.s_blocks_count_hi;	/* Blocks count high 32bits */
-			sb.s_r_blocks_count_hi;	/* Reserved blocks count high 32 bits*/
-			sb.s_free_blocks_hi; 	/* Free blocks count */
-			sb.s_min_extra_isize;	/* All inodes have at least # bytes */
-			sb.s_want_extra_isize; 	/* New inodes should reserve # bytes */
-			sb.s_flags;		/* Miscellaneous flags */
-			sb.s_raid_stride;		/* RAID stride */
-			sb.s_mmp_update_interval;  /* # seconds to wait in MMP checking */
-			sb.s_mmp_block;            /* Block for multi-mount protection */
-			sb.s_raid_stripe_width;    /* blocks on all data disks (N*stride)*/
-			sb.s_log_groups_per_flex;	/* FLEX_BG group size */
-			sb.s_checksum_type;	/* metadata checksum algorithm */
-			sb.s_encryption_level;	/* versioning level for encryption */
-	sb.s_reserved_pad;		/* Padding to next 32bits */
-	sb.s_kbytes_written;	/* nr of lifetime kilobytes written */
-	sb.s_snapshot_inum;	/* Inode number of active snapshot */
-	sb.s_snapshot_id;		/* sequential ID of active snapshot */
-	sb.s_snapshot_r_blocks_count; /* reserved blocks for active
+			sb.s_journal_uuid[16] = 0;	/* uuid of journal superblock */				
+			sb.s_journal_inum = 0;		/* inode number of journal file */
+			sb.s_journal_dev = 0;		/* device number of journal file */
+			sb.s_last_orphan = 0;		/* start of list of inodes to delete */
+			sb.s_hash_seed[4] = 0;		/* HTREE hash seed */
+			sb.s_def_hash_version = 0;	/* Default hash version to use */
+			sb.s_jnl_backup_type = 0; 	/* Default type of journal backup */		
+			sb.s_desc_size = 0;		/* Group desc. size: INCOMPAT_64BIT */
+			sb.s_default_mount_opts = 0;
+			sb.s_first_meta_bg = 0;	/* First metablock group */
+			sb.s_mkfs_time = 0;		/* When the filesystem was created */
+			sb.s_jnl_blocks[17] = 0; 	/* Backup of the journal inode */
+			sb.s_blocks_count_hi = 0;	/* Blocks count high 32bits */
+			sb.s_r_blocks_count_hi = 0;	/* Reserved blocks count high 32 bits*/
+			sb.s_free_blocks_hi = 0; 	/* Free blocks count */
+			sb.s_min_extra_isize = 0;	/* All inodes have at least # bytes */
+			sb.s_want_extra_isize = 0; 	/* New inodes should reserve # bytes */
+			sb.s_flags = 0;		/* Miscellaneous flags */
+			sb.s_raid_stride = 0;		/* RAID stride */
+			sb.s_mmp_update_interval = 0;  /* # seconds to wait in MMP checking */
+			sb.s_mmp_block = 0;            /* Block for multi-mount protection */
+			sb.s_raid_stripe_width = 0;    /* blocks on all data disks (N*stride)*/
+			sb.s_log_groups_per_flex = 0;	/* FLEX_BG group size */
+			sb.s_checksum_type = 0;	/* metadata checksum algorithm */
+			sb.s_encryption_level = 0;	/* versioning level for encryption */
+			sb.s_reserved_pad = 0;		/* Padding to next 32bits */
+			sb.s_kbytes_written = 0;	/* nr of lifetime kilobytes written */
+			sb.s_snapshot_inum = 0;	/* Inode number of active snapshot */
+			sb.s_snapshot_id = 0;		/* sequential ID of active snapshot */
+			sb.s_snapshot_r_blocks_count = 0; /* reserved blocks for active
 					      snapshot's future use */
-	sb.s_snapshot_list;	/* inode number of the head of the on-disk snapshot list */
-#define EXT4_S_ERR_START ext4_offsetof(struct ext2_super_block, s_error_count)
+			sb.s_snapshot_list = 0;	/* inode number of the head of the on-disk snapshot list */
 	
-	sb.s_error_count;		/* number of fs errors */
-	sb.s_first_error_time;	/* first time an error happened */
-	sb.s_first_error_ino;	/* inode involved in first error */
-	sb.s_first_error_block;	/* block involved of first error */
-	sb.s_first_error_func[32];	/* function where the error happened */
-	sb.s_first_error_line;	/* line number where error happened */
-	sb.s_last_error_time;	/* most recent time of an error */
-	sb.s_last_error_ino;	/* inode involved in last error */
-	sb.s_last_error_line;	/* line number where error happened */
-	sb.s_last_error_block;	/* block involved of last error */
-	sb.s_last_error_func[32];	/* function where the error happened */
+			sb.s_error_count = 0;		/* number of fs errors */
+			sb.s_first_error_time = 0;	/* first time an error happened */
+			sb.s_first_error_ino = 0;	/* inode involved in first error */
+			sb.s_first_error_block = 0;	/* block involved of first error */
+			sb.s_first_error_func[32] = 0;	/* function where the error happened */
+			sb.s_first_error_line = 0;	/* line number where error happened */
+			sb.s_last_error_time = 0;	/* most recent time of an error */
+			sb.s_last_error_ino = 0;	/* inode involved in last error */
+			sb.s_last_error_line = 0;	/* line number where error happened */
+			sb.s_last_error_block = 0;	/* block involved of last error */
+			sb.s_last_error_func[32] = 0;	/* function where the error happened */
 #define EXT4_S_ERR_END ext4_offsetof(struct ext2_super_block, s_mount_opts)
 	
 
-	sb.s_mount_opts[64];
-	sb.s_usr_quota_inum;	/* inode number of user quota file */
-	sb.s_grp_quota_inum;	/* inode number of group quota file */
-	sb.s_overhead_blocks;	/* overhead blocks/clusters in fs */
-	sb.s_backup_bgs[2];	/* If sparse_super2 enabled */
-	sb.s_encrypt_algos[4];	/* Encryption algorithms in use  */
-	sb.s_encrypt_pw_salt[16];	/* Salt used for string2key algorithm */
-	sb.s_lpf_ino;		/* Location of the lost+found inode */
-	sb.s_prj_quota_inum;	/* inode for tracking project quota */
-	sb.s_checksum_seed;	/* crc32c(orig_uuid) if csum_seed set */
-	sb.s_reserved[98];		/* Padding to the end of the block */
-	sb.s_checksum;		/* crc32c(superblock) */
+			sb.s_mount_opts[64] = 0;
+			sb.s_usr_quota_inum = 0;	/* inode number of user quota file */
+			sb.s_grp_quota_inum = 0;	/* inode number of group quota file */
+			sb.s_overhead_blocks = 0;	/* overhead blocks/clusters in fs */
+			sb.s_backup_bgs[2] = 0;	/* If sparse_super2 enabled */
+			sb.s_encrypt_algos[4] = 0;	/* Encryption algorithms in use  */
+			sb.s_encrypt_pw_salt[16] = 0;	/* Salt used for string2key algorithm */
+			sb.s_lpf_ino = 0;		/* Location of the lost+found inode */
+			sb.s_prj_quota_inum = 0;	/* inode for tracking project quota */
+			sb.s_checksum_seed = 0;	/* crc32c(orig_uuid) if csum_seed set */
+			sb.s_reserved[98] = 0;		/* Padding to the end of the block */
+			sb.s_checksum = 0;		/* crc32c(superblock) */
 
+`			write(fd, sb, sizeof(ext2_super_block));
+			
 // for group 0
-	int skip;
+			int skip;
 	
 			skip = 32768 * i;
 			bgdesc.bg_block_bitmap = skip + 1 + 1 + sb.s_reserved_gdt_blocks;
@@ -185,7 +196,7 @@ int main(int argc, char *argv[]) {
 			bgdesc.bg_inode_table = bgdesc.bg_block_bitmap + 2;
 							
 			if (i == 0){
-				bgdesc.bg_free_blocks_count = sb.s_blocks_per_group - ;		
+				bgdesc.bg_free_blocks_count = sb.s_blocks_per_group;		
 				bgdesc.bg_used_dirs_count = 2;
 				bgdesc.bg_free_inodes_count = sb.s_inodes_count - 11;
 			}			
@@ -205,20 +216,20 @@ int main(int argc, char *argv[]) {
 	}
 // need to define inode no
 	if (inode_no == 2){	
-		inodes.i_mode = 16877;
+		 inodes.i_mode = 16877;
 --		inodes.i_size = 1 * bs;		
-		inodes.i_atime;	
-		inodes.i_ctime;	
-		inodes.i_mtime;	
+		inodes.i_atime = time(NULL);	
+		inodes.i_ctime = time(NULL);	
+		inodes.i_mtime = time(NULL);	
 		inodes.i_links_count = 3;	
 		inodes.i_blocks = inodes.i_size / 512;	
 	}
 	if (inode_no == 11){	
 		inodes.i_mode = 16832;
 		inodes.i_size = 4 * bs;		
-		inodes.i_atime;	
-		inodes.i_ctime;	
-		inodes.i_mtime;	
+		inodes.i_atime = time(NULL);	
+		inodes.i_ctime = time(NULL);	
+		inodes.i_mtime = time(NULL);	
 		inodes.i_links_count = 2;	
 		inodes.i_blocks = inodes.i_size / 512;	
 	}
@@ -235,8 +246,16 @@ int main(int argc, char *argv[]) {
 			inodes.h_i_translator = 0;
 		} hurd1;
 	} osd1;				
-	inodes.i_block[EXT2_N_BLOCKS] = 643;    // 2.15
-	644-647
+	
+	if (inode_no == 2)				
+		inodes.i_block[0] = 1 + 1 + sb.s_reserved_gdt_blocks + 1 + 1 + ((sb.s_inodes_per_group * sb.s_inode_size) / bs);    // 2.15
+	else if (inode_no == 11){
+		inodes.i_block[0] = 1 + 1 + sb.s_reserved_gdt_blocks + 1 + 1 + ((sb.s_inodes_per_group * sb.s_inode_size) / bs) + 1;    
+		inodes.i_block[1] = inodes.i_block[0] + 1;	
+		inodes.i_block[2] = inodes.i_block[0] + 2;	
+		inodes.i_block[3] = inodes.i_block[0] + 3;
+	}
+
 	inodes.i_generation = 0;	
 	inodes.i_file_acl = 0;	
 	inodes.i_size_high = 0;
